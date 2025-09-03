9a7a9b8420274914bc26c1f75069c37a135df5c8
DrkNxt <65965412+DrkNxt@users.noreply.github.com>
2025-09-03 14:35:09 +0200

    Add game UI

--- script.js
+++ script.js
@@ -1,40 +1,55 @@
-let playerScore = 0
-let computerScore = 0
+const results = document.querySelector("#results");
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
-    const choices = ["rock", "paper", "scissors"]
-    return choices[Math.floor(Math.random() * choices.length)]
+    const choices = ["rock", "paper", "scissors"];
+    return choices[Math.floor(Math.random() * choices.length)];
 }
 
 function getPlayerChoice() {
-    return prompt("rock, paper or ,scissors?")
+    return prompt("rock, paper or ,scissors?");
 }
 
-function playRound() {
-    let computerChoice = getComputerChoice()
-    let playerChoice = getPlayerChoice().toLowerCase()
-    if (playerChoice === "rock" && computerChoice === "scissors" || playerChoice === "paper" && computerChoice === "rock" || playerChoice === "scissors" && computerChoice === "paper"){
-        playerScore++
-        console.log("You win! " + playerChoice + " beats " + computerChoice)
-    } else if (computerChoice === "rock" && playerChoice === "scissors" || computerChoice === "paper" && playerChoice === "rock" || computerChoice === "scissors" && playerChoice === "paper"){
-        computerScore++
-        console.log("You Lose! " + computerChoice + " beats " + playerChoice)
+function playRound(playerChoice) {
+    let computerChoice = getComputerChoice();
+    if (playerChoice === "rock" && computerChoice === "scissors" || 
+            playerChoice === "paper" && computerChoice === "rock" || 
+            playerChoice === "scissors" && computerChoice === "paper"){
+        playerScore++;
+        results.innerHTML = `You win! ${playerChoice} beats ${computerChoice}
+            </br>Current score: You ${playerScore}-${computerScore} COM`;
+    } else if (computerChoice === "rock" && playerChoice === "scissors" || 
+                computerChoice === "paper" && playerChoice === "rock" || 
+                computerChoice === "scissors" && playerChoice === "paper"){
+        computerScore++;
+        results.innerHTML = `You lose! ${computerChoice} beats ${playerChoice}
+            </br>Current score: You ${playerScore}-${computerScore} COM`;
     }else {
-        console.log("Draw! You both picked " + computerChoice)
+        results.innerHTML = `Draw! You both picked ${computerChoice}
+            </br>Current score: You ${playerScore}-${computerScore} COM`;
+    }
+    if (playerScore >= 5) {
+        results.innerHTML += `<br>You win the game!`;
+        playerScore = 0;
+        computerScore = 0;
+    }else if (computerScore >= 5) {
+        results.innerHTML += `<br>You lose the game!`;
+        playerScore = 0;
+        computerScore = 0;
     }
 }
 
-function playGame() {
-    playerScore = 0
-    computerScore = 0
-    for (let i = 0; i<5; i++) {
-        playRound()
+document.addEventListener("click", e => {
+    switch (e.target.id) {
+        case "rock":
+            playRound("rock");
+            break;
+        case "paper":
+            playRound("paper");
+            break;
+        case "scissors":
+            playRound("scissors");
+            break;
     }
-    if (playerScore > computerScore) {
-        console.log("You win! Score: " + playerScore + " : " + computerScore +"!")
-    }else if (playerScore < computerScore) {
-        console.log("You lose! Score: " + playerScore + " : " + computerScore +"!")
-    }else {
-        console.log("Draw! Score: " + playerScore + " : " + computerScore +"!")
-    }
-}
+});
